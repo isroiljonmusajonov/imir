2d982a62d36ab0bfc402c5c45c36921f6fcdf907
isroiljonmusajonov <isroiljonmusaj@gmail.com>
2021-11-04 22:15:27 +0500

    sarusel qoshdim

--- app/app.js
+++ app/app.js
@@ -1,21 +1,19 @@
 const upIcon = document.querySelectorAll(".up");
-const h2 = document.getElementById("h2");
-const but = document.getElementById("but");
-const buts = document.getElementById("buts");
+const h2 = document.querySelectorAll("#h2");
+const but = document.querySelectorAll("#but");
+const buts = document.querySelectorAll("#buts");
 
-but.addEventListener("click", function () {
-  h2.innerText = +h2.innerText - 1;
+but.forEach((but, i) => {
+  but.addEventListener("click", function () {
+    h2[i].innerText = +h2[i].innerText - 1;
+  });
 });
-buts.addEventListener("click", function () {
-  h2.innerText = +h2.innerText + 1;
+buts.forEach((buts, i) => {
+  buts.addEventListener("click", function () {
+    h2[i].innerText = +h2[i].innerText + 1;
+  });
 });
-// upIcon.addEventListener("click", () => {
-//   window.scroll({
-//     top: 0,
-//     left: 0,
-//     behavior: "smooth",
-//   });
-// });
+
 console.log("skal");
 const menuBtn = document.querySelector(".menu-icon span");
 const searchBtn = document.querySelector(".search-icons");
@@ -41,3 +39,64 @@ searchBtn.onclick = () => {
   searchBtn.classList.add("hide");
   cancelBtn.classList.add("show");
 };
+let divbuton = document.querySelector(".div");
+const btns = document.querySelectorAll(".Up");
+const salom = document.querySelector(".submit");
+const qayt = document.querySelector("#qayt");
+console.log(qayt);
+btns.forEach((btns) => {
+  btns.addEventListener("click", () => {
+    divbuton.classList.add("active");
+  });
+});
+// btns[0].addEventListener("click", () => {
+//   divbuton.classList.add("active");
+// });
+// btns[1].addEventListener("click", () => {
+//   divbuton.classList.add("active");
+// });
+// btns.forEach(() => {
+//   btns[0].addEventListener("click", () => {
+//     divbuton.classList.add("active");
+//   });
+// });
+// btns[2].addEventListener("click", () => {
+//   divbuton.classList.add("active");
+// });
+// btns[3].addEventListener("click", () => {
+//   divbuton.classList.add("active");
+// });
+
+qayt.addEventListener("click", () => {
+  divbuton.classList.remove("active");
+});
+console.log(salom);
+const boxes = document.querySelectorAll(".contact-box");
+window.addEventListener("scroll", checkBoxes);
+checkBoxes();
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+  boxes.forEach((box) => {
+    const boxtop = box.getBoundingClientRect().top;
+    if (boxtop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
+
+// const boxess = document.querySelectorAll(".woman-box1");
+// window.addEventListener("scroll", checkBoxes);
+// checkBoxes();
+// function checkBoxes() {
+//   const triggerBottom = (window.innerHeight / 5) * 4;
+//   boxess.forEach((box) => {
+//     const boxtop = box.getBoundingClientRect().top;
+//     if (boxtop < triggerBottom) {
+//       box.classList.add("show");
+//     } else {
+//       box.classList.remove("show");
+//     }
+//   });
+// }
